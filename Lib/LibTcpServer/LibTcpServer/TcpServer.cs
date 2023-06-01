03f7fd06813e21ac1cc42f2179be25d456b4417c
izhor <salomuz@naver.com>
2023-06-02 07:12:58 +0900

    MulticastTcp Server library
    Multicast Tcp Server library!

---
+++ Lib/LibTcpServer/LibTcpServer/TcpServer.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//Example
+// Example for Multicast Tcp Server
+//public class MulticastTcpSession : TcpSession
+//{
+//    public MulticastTcpSession(TcpServer server) :base (server) {}
+
+//    protected override async Task AsyncOnReceived(byte[] buffer, long offset, long length)
+//    {
+//         Processing processing = new Processing();
+
+//         bool result = await processing.RequestfromDB(this, buffer, offset, length);
+//    }
+//}
+
+//public class MulticastServer: TcpServer
+//{
+//    public MulticastServer(string IPAddress, int port) : base(IPAddress, port) { }
+//    public MulticastServer(int port) : base(port) { }
+
+//    protected override TcpSession CreateSession()
+//    {
+//        return new MulticastTcpSession(this);
+//    }
+//}
+
+namespace LibTcpServer
+{
+    public class TcpServer :IDisposable
+    {
+        /// <summary>
+        /// Server Id
+        /// </summary>
+        public Guid Id { get; }
+
+        /// <summary>
+        /// Tcp Server Tcp IpAddress
+        /// </summary>
+        public string IpAddress { get; }
+        /// <summary>
+        /// Tcp Server Port
+        /// </summary>
+        public int Port { get; }
+        
+        /// <summary>
+        /// Endpoint
+        /// </summary>
+        public EndPoint EndPoint { get; private set; }
+
+        /// <summary>
+        /// Is server started?
+        /// </summary>
+        public bool IsStarted { get; private set; }
+
+        /// <summary>
+        /// Is the server accepting new clients?
+        /// </summary>
+        public bool IsAccepting { get; private set; }
+
+        /// <summary>
+        /// Server acceptor
+        /// </summary>
+        private TcpListener _acceptorClient;
+
+        /// <summary>
+        /// Invoke Server user from receiving data
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="offset"></param>
+        /// <param name="length"></param>
+        public delegate void BufferReceived(byte[] bytes, int offset, int length);
+
+        /// <summary>
+        /// Event 
+        /// </summary>
+        public event BufferReceived eventBufferReceived;
+        
+        /// <summary>
+        /// Client count that is connected to server;
+        /// </summary>
+        private int Client_Count { get; set; } = 0;
+
+        /// <summary>
+        /// Create Socket 
+        /// </summary>
+        /// <returns></returns>
+        protected  TcpListener CreateListener()
+        {
+            return new TcpListener(IPAddress.Parse(IpAddress), Port);
+        }
+        
+        /// <summary>
+        /// Tpc Server requests a address and port number
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="port"></param>
+        public TcpServer(string address, int port) : this ( new IPEndPoint( IPAddress.Parse(address), port), address, port) {}
+
+        /// <summary>
+        /// Tcp Server request a port number
+        /// </summary>
+        /// <param name="port"></param>
+        public TcpServer(int port) : this(IPAddress.Any.ToString(), port) {}
+
+        /// <summary>
+        /// Tcp Server requests endpoint, ipaddress and port number
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="address"></param>
+        /// <param name="port"></param>
+        public TcpServer(IPEndPoint endpoint, string address, int port)
+        {
+            Id = Guid.NewGuid();
+            IpAddress = address;
+            Port = port;
+            EndPoint = endpoint; 
+        }
+        
+        /// <summary>
+        /// Start Server
+        /// </summary>
+        /// <returns></returns>
+        public async Task Start()
+        {
+            Debug.Assert(!IsStarted, "TCPServer is already started!");
+
+            if (IsStarted)
+                return;
+            // Initialize a client acceptor 
+            _acceptorClient = null;
+
+            try
+            {
+                //Create a new accepter
+                _acceptorClient = CreateListener();
+
+                //Bind the acceptor socket to the endpoint
+                _acceptorClient.Start();
+
+                Console.WriteLine("Server is started. \nListening for incoming connections...");
+                IsAccepting = true;
+
+                while (IsAccepting)
+                {
+                    TcpClient tcpClient = await _acceptorClient.AcceptTcpClientAsync();
+
+                    _ = ProcessAccept(tcpClient);
+                    Console.WriteLine($"Client {Client_Count.ToString()} : " + tcpClient.Client.RemoteEndPoint.ToString());
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+                IsAccepting = false;
+            }
+        }
+
+        /// <summary>
+        /// Stop Tcp Server
+        /// </summary>
+        public void Stop()
+        {
+            Debug.Assert(IsStarted, "TCP server isn't started!");
+            if (!IsStarted)
+                return;
+
+            _acceptorClient.Stop();
+            IsStarted = false;
+            Dispose();
+        }
+        /// <summary>
+        /// Start to Accept clients
+        /// </summary>
+        /// <param name="tcpClient"></param>
+        /// <returns></returns>
+        private async Task ProcessAccept(TcpClient tcpClient)
+        {
+            try
+            {
+                //Create a session
+                var session = CreateSession();
+
+                // Register the session
+                RegisterSession(session);
+
+                // Start to Connect;
+                await session.Connect(tcpClient);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                
+        }
+
+        #region  Session Menegment
+        
+        /// <summary>
+        /// Add the sessions with Id to Container
+        /// </summary>
+        protected readonly ConcurrentDictionary<Guid, TcpSession> Sessions = new ConcurrentDictionary<Guid, TcpSession>();
+        
+        /// <summary>
+        /// Create a new session
+        /// </summary>
+        /// <returns></returns>
+        protected virtual TcpSession CreateSession()
+        {
+            return new TcpSession(this);
+        }
+
+        /// <summary>
+        /// Register the session
+        /// </summary>
+        /// <param name="session"></param>
+        internal void RegisterSession(TcpSession session)
+        {
+            // Register a new session
+            if(Sessions.TryAdd(session.Id, session))
+               Client_Count++;
+        }
+
+        /// <summary>
+        /// Find the session fro, container
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        public TcpSession FindSession(Guid guid)
+        {
+            return Sessions.TryGetValue(guid, out TcpSession session) ? session : null;
+        }
+
+        /// <summary>
+        /// Unregister the session from container
+        /// </summary>
+        /// <param name="guid"></param>
+        public void UnregisterSession(Guid guid)
+        {
+            if(Sessions.TryRemove(guid, out TcpSession _))
+                Client_Count--;
+        }
+
+        public virtual bool DisconnectAll()
+        {
+            if(!IsStarted)
+                return false;
+
+            // Disconnect all sessions
+            foreach (var session in Sessions.Values)
+                session.Disconnect();
+            
+            return true;
+        }
+        #endregion
+
+        #region Option setting
+        /// <summary>
+        /// This option will set the listening socket's blacklog size
+        /// </summary>
+        public int OptionAcceptorBackLog { get; set; } = 1024;
+
+        /// <summary>
+        /// Option: Receive buffer size from client
+        /// </summary>
+        public int OptionReceiveBufferSize { get; set; } = 8192;
+        /// <summary>
+        /// Option: Send buffer size to client
+        /// </summary>
+        public int OptionSendBufferSize { get; set; } = 8192;
+        #endregion
+
+        #region  Server handlers
+
+        protected virtual void OnDataReceived(byte[] bytes, int offset, int length) { eventBufferReceived?.Invoke(bytes, offset, length); }
+        
+        /// <summary>
+        /// Handle session connection notification
+        /// </summary>
+        /// <param name="session"></param>
+        /// <remarks>
+        /// Notify if A session is created and connected 
+        /// </remarks>
+        protected virtual void OnSessionConnected(TcpSession session) { }
+
+        /// <summary>
+        /// Handle session disconnection notification
+        /// </summary>
+        /// <param name="session"></param>
+        /// <remarks>
+        /// Notify if A session is disconnected 
+        /// </remarks>
+        protected virtual void OnSessionDisconnected(TcpSession session) { }
+
+        internal void OnSessionConnectedInternal(TcpSession session) { OnSessionConnected(session); }
+        internal void OnSessionDisconnectedInternal(TcpSession session) { OnSessionDisconnected(session); }
+        internal void OnSessionOnDataReceivedInternal(byte[] bytes, int offset, int length) { OnDataReceived(bytes, offset, length); }
+        #endregion
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Lib/LibTcpServer/LibTcpServer/TcpSession.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibTcpServer
+{
+    public class TcpSession : IDisposable
+    {
+        /// <summary>
+        /// Tcp Server 
+        /// </summary>
+        private TcpServer Server { get; }
+
+        /// <summary>
+        /// Set tcpClient to Session
+        /// </summary>
+        public TcpClient Client { get; private set; }
+
+        /// <summary>
+        /// Session ID
+        /// </summary>
+        public Guid Id { get; }
+
+        /// <summary>
+        /// Is the session connected?
+        /// </summary>
+        public bool IsConnected { get; private set; }
+
+        /// <summary>
+        /// Check if buffer from a client is receiving or not?
+        /// </summary>
+        private bool _receiving;
+
+        /// <summary>
+        /// Received buffer from client;
+        /// </summary>
+        private byte[] ReceivedBuffer;
+
+        /// <summary>
+        /// Send buffer to client
+        /// </summary>
+        private byte[] SendBuffer;
+
+        /// <summary>
+        /// Initialize the session wiht given server
+        /// </summary>
+        /// <param name="server"></param>
+        public TcpSession(TcpServer server)
+        {
+            Id = Guid.NewGuid();
+            Server = server;
+            OptionReceiveBufferSize = server.OptionReceiveBufferSize;
+            OptionSendBufferSize = server.OptionSendBufferSize;
+        }
+
+        /// <summary>
+        /// Connect the session with tcpclient
+        /// </summary>
+        /// <param name="tcpClient"></param>
+        /// <returns></returns>
+        internal async Task Connect(TcpClient tcpClient)
+        {
+            Client = tcpClient;
+
+            IsConnected = true;
+
+            //Inform to server the client is connected
+            Server.OnSessionConnectedInternal(this);
+
+            await TryAsyncReceive();
+        }
+
+        /// <summary>
+        /// Disconnect the session 
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool Disconnect()
+        {
+            if (!IsConnected)
+                return false;
+
+            // Unregister session
+            Server.UnregisterSession(Id);
+
+            //Close the client connection
+            Client.Close();
+            
+            // is CLient connected ?
+            IsConnected = false;
+
+            _receiving = false;
+
+            Client.Dispose();
+            Dispose();
+            return true;
+        }
+
+        #region Send / Receive management
+
+        /// <summary>
+        /// Try Async receive buffer from client
+        /// </summary>
+        /// <returns></returns>
+        private async Task TryAsyncReceive()
+        {
+            if (_receiving)
+                return;
+
+            if (!IsConnected)
+                return;
+
+            try
+            {
+                _receiving = true;
+
+                using (NetworkStream stream = Client.GetStream())
+                {
+                    while (Client.Connected)
+                    {
+                        ReceivedBuffer = new byte[OptionReceiveBufferSize];
+
+                        int bytesRead = await stream.ReadAsync(ReceivedBuffer, 0, ReceivedBuffer.Length);
+
+                        if (bytesRead == 0)
+                        {
+                            Console.WriteLine("bytesRead is Zero " + Id.ToString());
+                            break;
+                        }
+                        
+                        _ = AsyncOnReceived(ReceivedBuffer, 0, bytesRead);
+                        OnReceived(ReceivedBuffer, 0, bytesRead);
+                        Server.OnSessionOnDataReceivedInternal(ReceivedBuffer, 0, bytesRead);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Client is disconnected, Id:" + this.Id.ToString() + "\n"+ex.Message);
+
+                //unregister the session
+                Server.UnregisterSession(Id);
+
+                _receiving = false;
+                IsConnected = false;
+
+                //Inform to server the client is disconnected
+                Server.OnSessionDisconnectedInternal(this);
+
+                Client.Dispose();
+                Dispose();
+            }
+            finally
+            {
+                Client.Close();
+            }
+
+        }
+
+        /// <summary>
+        /// Handle Received Buffer
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="length"></param>
+        /// <remarks>
+        /// async: This is called asynchrounly when buffer was received from the client
+        /// </remarks>
+        protected virtual async Task AsyncOnReceived(byte[] buffer, long offset, long length) { await Task.CompletedTask; }
+
+        /// <summary>
+        /// Handle Received buffer
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="length"></param>
+        /// <remarks>
+        /// void: This is called when buffer was received from the client
+        /// </remarks>
+        protected virtual void OnReceived(byte[] buffer, long offset, long length) {}
+        /// <summary>
+        /// Send the text to cleint
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public  void Send(string text) => Send(Encoding.ASCII.GetBytes(text), 0, (long)Encoding.ASCII.GetBytes(text).Length);
+
+        public void Send(byte[] bytes) => Send(bytes, 0, bytes.Length);
+
+        /// <summary>
+        /// Send data to the client (asynchronous)
+        /// </summary>
+        /// <param name="buffer">Buffer to send as a span of bytes</param>
+        /// <returns>'true' if the data was successfully sent, 'false' if the session is not connected</returns>
+        public void Send(byte[] buffer, int offset, long size)
+        {
+            if (buffer.Length == 0)
+                return;
+
+            SendBuffer = new byte[size];
+            buffer.CopyTo(SendBuffer, size);
+
+           _ = TryAsyncSend();
+        }
+
+        /// <summary>
+        /// Try to async send buffer to the clients
+        /// </summary>
+        /// <returns></returns>
+        private async Task TryAsyncSend()
+        {
+            if (!IsConnected)
+                return;
+
+            try
+            {
+                NetworkStream stream = Client.GetStream();
+                    if (Client.Connected)
+                    {
+                       await stream.WriteAsync(SendBuffer, 0, SendBuffer.Length);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Client is disconnected." + Id.ToString());
+                        Server?.OnSessionDisconnectedInternal(this);
+                        IsConnected = false;
+                        return;
+                    }
+
+                    SendBuffer = null;
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(this.Id.ToString() + " - "+ ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region Option Setting
+        /// <summary>
+        /// Option: Receive buffer size from client
+        /// </summary>
+        public int OptionReceiveBufferSize { get; set; } = 1024;
+        
+        /// <summary>
+        /// Option: Send buffer size to client
+        /// </summary>
+        public int OptionSendBufferSize { get; set; }
+        #endregion
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
